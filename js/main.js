5a7f816f51d8220b27d62eec595fcc2990a98f0a
Hazem_Genady <hazem.genady.18@gmail.com>
2026-04-19 20:40:18 +0200

    Adding MetaData and active classes

--- js/main.js
+++
@@ -1,7 +0,0 @@
-let page = window.location.pathname.split("/").pop();
-document.querySelectorAll(".nav-links a, .footer-links a, .sign a").forEach(link => {
-    let linkPage = link.getAttribute("href").split("/").pop();
-    if (linkPage === page) {
-        link.classList.add("active");
-    }
-});
