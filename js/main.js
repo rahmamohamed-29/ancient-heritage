220fecc5f3a9e826a7a811e7a06b4cb0ec729e95
Hazem_Genady <hazem.genady.18@gmail.com>
2026-04-25 23:33:24 +0300

    Finishing profile icon when sign up

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let user = JSON.parse(localStorage.getItem("user"));
-let Button = document.getElementById("sign");
+let Button = document.getElementById("sign-btn");
 let Icon = document.getElementById("icon");
 if(user){
     Icon.style.display="block";
